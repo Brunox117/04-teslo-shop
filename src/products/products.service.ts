a97ccfa308ccc1e0bfd35af5aafbb9c54db2565b
Bruno Jimenez <omarjm0910@gmail.com>
2025-08-30 10:27:08 -0600

    feat: Add pagination in products findAll methods

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,6 +10,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Injectable()
 export class ProductsService {
@@ -29,9 +30,13 @@ export class ProductsService {
     }
   }
 
-  findAll() {
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
     try {
-      return this.productRepository.find();
+      return this.productRepository.find({
+        take: limit,
+        skip: offset,
+      });
     } catch (error) {
       this.handleDBExceptions(error);
     }
