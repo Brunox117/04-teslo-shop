e6c0365c40b1f3f2e4df8920f2f87bda271e59f2
Bruno Jimenez <omarjm0910@gmail.com>
2025-08-30 10:39:30 -0600

    feat: add find one by slug

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -11,6 +11,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { isUUID } from 'class-validator';
 
 @Injectable()
 export class ProductsService {
@@ -42,11 +43,16 @@ export class ProductsService {
     }
   }
 
-  async findOne(id: string) {
+  async findOne(term: string) {
     try {
-      const product = await this.productRepository.findOneBy({ id });
+      let product: Product | null;
+      if (isUUID(term)) {
+        product = await this.productRepository.findOneBy({ id: term });
+      } else {
+        product = await this.productRepository.findOneBy({ slug: term });
+      }
       if (!product)
-        throw new NotFoundException(`Product with id ${id} not found`);
+        throw new NotFoundException(`Product with id ${term} not found`);
       return product;
     } catch (error) {
       this.handleDBExceptions(error);
